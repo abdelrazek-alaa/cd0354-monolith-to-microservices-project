12610fbffcc48e2c2fb4f3a4c52674bb22bc606a
abdelrazek <abdelrazekhdd@gmail.com>
2022-08-14 22:19:43 +0200

    Container Orchestration with Kubernetes

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://ab6f826eedff1469082a7418336f0d30-546060080.us-west-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
